3323677875381232374ebfee6c3f09e9d42ceb11
lixiaochun <lixiaochun01@baidu.com>
2023-01-09 16:26:13 +0800

    delete node_modules

--- node_modules/@vue/reactivity/index.ts
+++
@@ -1,3 +0,0 @@
-import  {isObject} from "@mini-vue3/shared"
-const obj = {name:'Vue3'}
-console.log(isObject(obj))
--- packages/reactivity/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var shared_1 = require("@mini-vue3/shared");
-var obj = { name: 'Vue3' };
-console.log(shared_1.isObject(obj));
